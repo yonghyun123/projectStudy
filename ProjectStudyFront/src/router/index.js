a4f0131b6f97ae4c9aafd91acb3410a9477c188d
yonghyun123 <model9960@naver.com>
2017-12-25 15:25:29 +0900

    initial setting about main components

--- ProjectStudyFront/src/router/index.js
+++ ProjectStudyFront/src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import VueGoodTable from 'vue-good-table';
+
+// components
+const Login = () => import('../components/Login');
+const Main = () => import('../components/Main');
+const Signup = () => import('../components/Signup');
 
 Vue.use(Router);
+Vue.use(VueGoodTable);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
-    },
-  ],
+const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/main',
+    name: 'Main',
+    component: Main,
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup,
+  },
+];
+
+const router = new Router({
+  routes,
 });
+
+export default router;
