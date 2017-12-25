6b6e576f49c5823a07aedb5e8d86eba02fe13da5
yonghyun123 <model9960@naver.com>
2017-12-25 13:45:54 +0900

    start projproject study

---
+++ ProjectStudyBack/.metadata/.plugins/org.eclipse.core.resources/.history/a0/109789dd30e6001716c98f8b4241b58f
@@ -0,0 +1,7 @@
+package com.yonghyun.projectStudy.service;
+
+import com.yonghyun.projectStudy.domain.Employee;
+
+public interface EmployeeService {
+	Employee getEmployee(int id);
+}
---
+++ ProjectStudyBack/ProjectStudy/src/main/java/com/yonghyun/projectStudy/domain/Employee.java
@@ -0,0 +1,14 @@
+package com.yonghyun.projectStudy.domain;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+	int empId;
+	int depId;
+	String empName;
+	String employeeRrn;
+	String empEdu;
+	String empEmail;
+	String pass;
+}
