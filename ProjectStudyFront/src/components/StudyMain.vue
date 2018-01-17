fbc84b3d7e5438dc12f183e93fc07a266cb961c9
yonghyun123 <model9960@naver.com>
2018-01-17 15:29:11 +0900

    vue안에서 image binding 성공

--- ProjectStudyFront/src/components/StudyMain.vue
+++ ProjectStudyFront/src/components/StudyMain.vue
@@ -4,14 +4,14 @@
     <main>
       <body>
       <div class="card-expansion">
-      <md-card>
+      <md-card v-for="item in items" :key="item">
         <md-card-media>
-          <img src="../assets/logo.png" @load="Resizing(this)">
+          <img :src="items.picture">
         </md-card-media>
 
-        <md-card-header>
-          <div class="md-title">Title goes here</div>
-          <div class="md-subhead">Subtitle here</div>
+        <md-card-header >
+          <div class="md-title">{{ items.title }}</div>
+          <div class="md-subhead"> {{ items.subttitle }}</div>
         </md-card-header>
 
         <md-card-expand>
@@ -25,66 +25,6 @@
               <md-button class="md-icon-button">
                 <md-icon>keyboard_arrow_down</md-icon>
               </md-button>
-            </md-card-expand-trigger>
-          </md-card-actions>
-
-          <md-card-expand-content>
-            <md-card-content>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
-            </md-card-content>
-          </md-card-expand-content>
-        </md-card-expand>
-      </md-card>
-            <md-card>
-        <md-card-media>
-          <img src="../assets/김용현 L.jpg" @onLoad="Resizing(this)">
-        </md-card-media>
-
-        <md-card-header>
-          <div class="md-title">Title goes here</div>
-          <div class="md-subhead">Subtitle here</div>
-        </md-card-header>
-
-        <md-card-expand>
-          <md-card-actions md-alignment="space-between">
-            <div>
-              <md-button>Action</md-button>
-              <md-button>Action</md-button>
-            </div>
-
-            <md-card-expand-trigger>
-              <md-button class="md-icon-button">
-                <md-icon>keyboard_arrow_down</md-icon>
-              </md-button>
-            </md-card-expand-trigger>
-          </md-card-actions>
-
-          <md-card-expand-content>
-            <md-card-content>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
-            </md-card-content>
-          </md-card-expand-content>
-        </md-card-expand>
-      </md-card>
-
-      <md-card>
-        <md-card-media>
-          <img src="../assets/황소.jpeg" @onLoad="Resizing(this)">
-        </md-card-media>
-
-        <md-card-header>
-          <div class="md-title">Title goes here</div>
-          <div class="md-subhead">Subtitle here</div>
-        </md-card-header>
-
-        <md-card-expand>
-          <md-card-actions md-alignment="space-between">
-            <div>
-              <md-button>Action</md-button>
-            </div>
-
-            <md-card-expand-trigger>
-              <md-button>Learn more</md-button>
             </md-card-expand-trigger>
           </md-card-actions>
 
@@ -104,8 +44,16 @@
 
 <script>
 const Header = () => import('../components/Header');
+
 export default {
   name: 'StudyMain',
+  data: () => ({
+    items: {
+      title: null,
+      subtitle: null,
+      picture: '../../src/assets/logo.png',
+    },
+  }),
   components: {
     studyHeader: Header,
   },
